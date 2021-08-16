8e0744f00449a6b406fab86a0e5aa08cc701ed71
Jason Montleon <jmontleo@redhat.com>
2021-08-16 09:23:12 -0400

    Get rid of entrypoint scripts

--- client-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-openvpn --config /openvpn.conf &
-
-socat TCP-LISTEN:443,fork,reuseaddr TCP:172.30.0.1:443
--- server-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir /tmp/openvpn
-
-openvpn --config /openvpn.conf &
-
-socat TCP-LISTEN:443,fork,reuseaddr TCP:192.168.123.6:443
